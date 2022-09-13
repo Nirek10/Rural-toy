ed8da7d4ec99c34c24e695deedeae36c31160214
Nirek10 <113040421+Nirek10@users.noreply.github.com>
2022-09-13 18:12:47 +0530

    Add files via upload
    This is my project work (NIREK M.M.) MYCAPTAIN

---
+++ MYCAPTAIN.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+r = float(input ("Input the radius of the circle : "))
+pi = 3.1416
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+# In[4]:
+
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+
+# In[ ]:
+
+
+
+
